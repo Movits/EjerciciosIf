5aee31da3f72e7f819f26f363cd9b6f11201499e
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-23 12:47:03 -0300

    agrege otra carpeta

--- Ej1/script.js
+++ Ej1/script.js
@@ -4,6 +4,8 @@
     + informar el resultado via web
     + agregar forma de recibir el usuario y contrasena
     + verificar si el usuaria que intenta loguarse esta registrado
+    + agregar otro usuario registrado
+    + ahora verificar todos los usuarios registrados
 
     Info?
     - nombre y contrasena de usuario
@@ -12,11 +14,13 @@
 
 const USUARIO_NOMBRE_REGISTRADO = "Roberto";
 const CONTRASENA_REGISTRADA = "Roberto1.";
+const USUARIO_NOMBRE_REGISTRADO_2 = "Juan";
+const CONTRASENA_REGISTRADA_2 = "juanito";
 
 let usuarioNombreIngresado = prompt("Ingrese Usuario");
 let usuarioContrasenaIngresada = prompt("Ingresar contrasena");
 
-if ((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO) && (usuarioContrasenaIngresada == CONTRASENA_REGISTRADA)) {
+if (((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO) && (usuarioContrasenaIngresada == CONTRASENA_REGISTRADA)) || ((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO_2) && (usuarioContrasenaIngresada == CONTRASENA_REGISTRADA_2)) ) {
     document.write("Usuario y contrasena correctos");
 }else{
     document.write("Usuario o contrasena incorrectos");
