1be0ef2e241e48ee1a9ff72807fe75dbd9edefaf
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-23 15:40:50 -0300

    agrege imagenes quando hace login o no

--- Ej1/script.js
+++ Ej1/script.js
@@ -17,11 +17,16 @@ const CONTRASENA_REGISTRADA = "Roberto1.";
 const USUARIO_NOMBRE_REGISTRADO_2 = "Juan";
 const CONTRASENA_REGISTRADA_2 = "juanito";
 
+const imgRes1 = document.querySelector("#img-verificada");
+const imgRes2 = document.querySelector("#img-no-verificada");
+
 let usuarioNombreIngresado = prompt("Ingrese Usuario");
 let usuarioContrasenaIngresada = prompt("Ingresar contrasena");
 
 if (((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO) && (usuarioContrasenaIngresada == CONTRASENA_REGISTRADA)) || ((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO_2) && (usuarioContrasenaIngresada == CONTRASENA_REGISTRADA_2)) ) {
-    document.write("Usuario y contrasena correctos");
+    imgRes1.src = "https://i.ebayimg.com/images/g/GbkAAOSwp2dd1oZ2/s-l1200.jpg";
+    imgRes2.style.display = "none";
 }else{
-    document.write("Usuario o contrasena incorrectos");
+    imgRes2.src = "https://clipart-library.com/data_images/49138.png";
+    imgRes1.style.display = "none";
 }
