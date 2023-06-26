f4b20901ca89f552cc7c1472ab9f33961de47418
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-26 18:03:52 -0300

    mejora UI

--- Ej2/script.js
+++ Ej2/script.js
@@ -1,12 +1,16 @@
 const VALOR_BTC_ETH = 13.5400;
 const VALOR_BTC_USD = 39685.40;
 
-let btcIngresado = prompt("Ingrese bitcoins a convertir");
-let conversionRequerida = confirm("Aceptar para ETH y cancelar para USD");
+function conversion() {
+    const valorIngresado = document.querySelector("#valor-ingresado");
+    const btcIngresado = valorIngresado.value;
+    const conversionRequerida = confirm("Aceptar para ETH y cancelar para USD");
 
-//conversionRequeriada == ture
-if (conversionRequerida) {
-    alert("Tu conversion es: " + (btcIngresado * VALOR_BTC_ETH));
-} else {
-    alert("Tu conversion es: " + (btcIngresado * VALOR_BTC_USD));
+    if (conversionRequerida) {
+        alert("Tu conversion es: " + (btcIngresado * VALOR_BTC_ETH));
+    } 
+    else {
+        alert("Tu conversion es: " + (btcIngresado * VALOR_BTC_USD));
+    }
 }
+    
