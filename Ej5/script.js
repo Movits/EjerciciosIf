16d37e9004392feeb912cc9877d1b5e36a64c618
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-28 00:07:34 -0300

    parte 3 finalizado

--- Ej5/script.js
+++ Ej5/script.js
@@ -2,17 +2,32 @@ const tipoMascota = document.querySelector("#tipoMascota");
 const cantidadMascotas = document.querySelector("#cantidadMascotas");
 const pResultado = document.querySelector("#resultado");
 const duracionPaseo = document.querySelector("#duracionPaseo");
-
+const pCosto = document.querySelector("#costo");
+const precios = {
+    perro: 1000,
+    gato: 800
+};
 
 function validarPaseo() {
     let Mascota = tipoMascota.value;
     let cantMascotas = cantidadMascotas.value;
     let duracion = duracionPaseo.value;
+    let costo = cantMascotas * precios[Mascota];
 
-    if (Mascota === "Perro" || Mascota === "Gato") {
+    if (Mascota === "perro" || Mascota === "gato") {
         if (cantMascotas > 0 && cantMascotas <= 3) {
             if (duracion >= 30 && duracion <= 60) {
                 pResultado.innerHTML = "Es posible realizar el paseo de " + cantMascotas + " " + Mascota + "(s) por " + duracion + " minutos.";
+
+                let precioPorMascota;
+                if (Mascota === "perro") {
+                    precioPorMascota = precios.perro;
+                } else {
+                    precioPorMascota = precios.gato;
+                }
+                let costo = cantMascotas * precioPorMascota;
+                
+                pCosto.innerHTML = "Costo total del paseo: " + costo;
             } else {
                 pResultado.innerHTML = "No es posible realizar el paseo, la duración debe ser entre 30 y 60 minutos.";
             }
@@ -20,6 +35,6 @@ function validarPaseo() {
             pResultado.innerHTML = "No es posible realizar el paseo, la cantidad de mascotas debe ser entre 1 y 3.";
         }
     } else {
-        pResultado.innerHTML = "Por favor, ingrese (Perro o Gato) como tipo de mascota.";
+        pResultado.innerHTML = "Por favor, ingrese (perro o gato) como tipo de mascota.";
     }
 }
