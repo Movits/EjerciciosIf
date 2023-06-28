379de7ecbc9ab1a97afbd186db7b2ba27f643a4a
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-28 15:54:38 -0300

    Ejercicio 5 finalizado

--- Ej5/script.js
+++ Ej5/script.js
@@ -1,51 +1,67 @@
-const tipoMascota = document.querySelector("#tipoMascota");
-const cantidadMascotas = document.querySelector("#cantidadMascotas");
+const cantidadPerros = document.querySelector("#cant-perros");
+const cantidadGatos = document.querySelector("#cant-gatos");
 const pResultado = document.querySelector("#resultado");
 const duracionPaseo = document.querySelector("#duracionPaseo");
 const pCosto = document.querySelector("#costo");
 const precios = {
     perro: 1000,
-    gato: 800
+    gato: 1000 * 1.7
 };
 
 function validarPaseo() {
-    let Mascota = tipoMascota.value;
-    let cantMascotas = cantidadMascotas.value;
+    let cantGatos = cantidadGatos.value;
+    let cantPerros = cantidadPerros.value;
     let duracion = duracionPaseo.value;
-    let tarifaPorMinutoPerro = 50;
-    let tarifaPorMinutoGato = 30;
+    let tarifaPorMinutoPerro = 10;
+    let tarifaPorMinutoGato = 20;
 
-    if (Mascota === "perro" || Mascota === "gato") {
-        if (cantMascotas > 0 && cantMascotas <= 3) {
-            if (duracion >= 30 && duracion <= 60) {
-                pResultado.innerHTML = "Es posible realizar el paseo de " + cantMascotas + " " + Mascota + "(s) por " + duracion + " minutos.";
+    if (cantPerros <= 2 && cantGatos <= 2 && cantPerros >= 0 && cantGatos >= 0) {
 
-                let precioPorMascota;
-                let tarifaPorMinuto;
-                if (Mascota === "perro") {
-                    precioPorMascota = precios.perro;
-                    tarifaPorMinuto = tarifaPorMinutoPerro;
-                } else {
-                    precioPorMascota = precios.gato;
-                    tarifaPorMinuto = tarifaPorMinutoGato;
+        if (duracion >= 30 && duracion <= 60) {
+            if (cantPerros > 0 || cantGatos > 0) {
+                let message = "Es posible realizar el paseo";
+                if (cantPerros > 0 && cantGatos > 0) {
+                    message += " de " + cantPerros + " perro(s) y " + cantGatos + " gato(s)";
+                } else if (cantPerros > 0) {
+                    message += " de " + cantPerros + " perro(s)";
+                } else if (cantGatos > 0) {
+                    message += " de " + cantGatos + " gato(s)";
                 }
-
+                message += " por " + duracion + " minutos.";
+                pResultado.innerHTML = message;
+    
                 let minutosExcedentes = 0;
                 if (duracion > 30) {
                     minutosExcedentes = duracion - 30;
                 }
-                let costoPorDuracion = minutosExcedentes * tarifaPorMinuto;
-                let costoTotal = (cantMascotas * precioPorMascota) + costoPorDuracion;
+    
+                let costoTotal = 0;
+
+                if (cantPerros > 0) {
+                    costoTotal += cantPerros * (precios.perro + minutosExcedentes * tarifaPorMinutoPerro);
+                }
+
+                if (cantGatos > 0) {
+                    costoTotal += cantGatos * (precios.gato + minutosExcedentes * tarifaPorMinutoGato);
+                }
 
                 pCosto.innerHTML = "Costo total del paseo: " + costoTotal;
+
             } else {
-                pResultado.innerHTML = "No es posible realizar el paseo, la duración debe ser entre 30 y 60 minutos.";
+                pResultado.innerHTML = "Debe ingresar al menos un perro o gato para realizar el paseo.";
                 pCosto.innerHTML = "";
             }
         } else {
-            pResultado.innerHTML = "No es posible realizar el paseo, la cantidad de mascotas debe ser entre 1 y 3.";
+            if (cantPerros > 0 || cantGatos > 0) {
+                pResultado.innerHTML = "No es posible realizar el paseo, la duración debe ser entre 30 y 60 minutos.";
+                pCosto.innerHTML = "";
+            } else {
+                pResultado.innerHTML = "Debe ingresar al menos un perro o gato y la duración debe ser entre 30 y 60 minutos.";
+                pCosto.innerHTML = "";
+            }
         }
+    
     } else {
-        pResultado.innerHTML = "Por favor, ingrese (perro o gato) como tipo de mascota.";
+        pResultado.innerHTML = "No es posible realizar el paseo. La cantidad de perros y gatos debe ser entre 0 y 2.";
     }
-}
+}    
