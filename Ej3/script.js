51c199a79ba33752f3c18cc7f46c59e03f5362be
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-27 21:53:22 -0300

    mejoras UI

--- Ej3/script.js
+++ Ej3/script.js
@@ -1,6 +1,12 @@
-let numeroRecibido = prompt("Ingrese 3 numeros");
+const numero1 = document.querySelector("#numero1");
+const numero2 = document.querySelector("#numero2");
+const numero3 = document.querySelector("#numero3");
+const pResultado = document.querySelector("#resultado");
 
-if (numeroRecibido.length === 3) {
+function verificarNumeros() {
+    let numeroRecibido = numero1.value + numero2.value + numero3.value;
+
+    if (numeroRecibido.length === 3) {
     let primerNum = numeroRecibido[0];
     let segundoNum = numeroRecibido[1];
     let terceroNum = numeroRecibido[2];
@@ -18,3 +24,7 @@ if (numeroRecibido.length === 3) {
 else {
     alert("Por favor, ingrese exactamente 3 numeros.");
 }
+}
+
+
+
