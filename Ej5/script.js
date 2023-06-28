e4143a4df070661b00ceba8c82560cd5f9d20435
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-27 23:29:35 -0300

    parte 2 finalizada

--- Ej5/script.js
+++ Ej5/script.js
@@ -1,15 +1,21 @@
 const tipoMascota = document.querySelector("#tipoMascota");
 const cantidadMascotas = document.querySelector("#cantidadMascotas");
 const pResultado = document.querySelector("#resultado");
+const duracionPaseo = document.querySelector("#duracionPaseo");
 
 
 function validarPaseo() {
     let Mascota = tipoMascota.value;
     let cantMascotas = cantidadMascotas.value;
+    let duracion = duracionPaseo.value;
 
     if (Mascota === "Perro" || Mascota === "Gato") {
         if (cantMascotas > 0 && cantMascotas <= 3) {
-            pResultado.innerHTML = "Es posible realizar el paseo de " + cantMascotas + " " + Mascota + "(s).";
+            if (duracion >= 30 && duracion <= 60) {
+                pResultado.innerHTML = "Es posible realizar el paseo de " + cantMascotas + " " + Mascota + "(s) por " + duracion + " minutos.";
+            } else {
+                pResultado.innerHTML = "No es posible realizar el paseo, la duración debe ser entre 30 y 60 minutos.";
+            }
         } else {
             pResultado.innerHTML = "No es posible realizar el paseo, la cantidad de mascotas debe ser entre 1 y 3.";
         }
